59796661c7581d09438f5d57f43f7a3982f23469
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    hammerhead: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- device.mk
+++ device.mk
@@ -230,6 +230,11 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Vibrator HAL
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
