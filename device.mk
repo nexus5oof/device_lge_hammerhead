55fde941df7760c493b87e0358ff3a7ecd5ee228
Bruno Martins <bgcngm@gmail.com>
2018-12-22 18:58:43 +0000

    hammerhead: Enable memory optimizations
     * Enable B-services aging propagation and delay service restart
       for devices with 2GB or less memory.
    
    Change-Id: I81a09cc8425602bfc586875b7d36a4f56a226908

--- device.mk
+++ device.mk
@@ -402,6 +402,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
 
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.vendor.qti.sys.fw.bservice_enable=true
+
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
