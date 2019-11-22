1cfef420e5cc44c5bdf1be188b239bd33be41e06
LuK1337 <priv.luk@gmail.com>
2019-11-22 15:35:26 +0100

    hammerhead: Exclude Snap overlays from RRO
    Change-Id: Ibefb7acaa158eea787618d1696057b75d2541e87

--- device.mk
+++ device.mk
@@ -20,7 +20,9 @@
 # Everything in this directory will become public
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/lineage-sdk
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/lineage-sdk \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 PRODUCT_COPY_FILES += \
     device/lge/hammerhead/init.hammerhead.rc:root/init.hammerhead.rc \
